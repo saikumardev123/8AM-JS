d214d6593434ce9f098c0d5adcd7d310f7757037
sai <sai@digital-lync.com>
2023-05-19 08:58:41 +0530

    18may

--- project/ecommerce/src/components/Cart.js
+++ project/ecommerce/src/components/Cart.js
@@ -1,11 +1,10 @@
+import { useState } from 'react';
 function Cart() {
-
+    const [state, setState] = useState(0);
     var cart;
-
     if (localStorage.getItem("cart") != null) {
         cart = JSON.parse(localStorage.getItem("cart"));
     }
-
     const removeFromCart = (id) => {
         var cart = JSON.parse(localStorage.getItem("cart"));
         cart.forEach((cartItem, i) => {
@@ -13,27 +12,27 @@ function Cart() {
                 cart.splice(i, 1);
             }
         })
-        alert("Item removed from Cart");
+        localStorage.setItem('cart', JSON.stringify(cart));
+        setState(state + 1);
     }
-
     const renderCartItems = () => {
         var list = cart.map(cartItem => {
 
-            return <tr>
-                <td class="col-sm-8 col-md-6">
-                    <div class="media">
-                        <a class="thumbnail pull-left" href="#"> <img class="media-object" src={cartItem.imageUrl} style={{ width: "72px", height: "72px" }} /> </a>
-                        <div class="media-body">
-                            <h4 class="media-heading">{cartItem.name}</h4>
+            return <tr key={cartItem.id}>
+                <td className="col-sm-8 col-md-6">
+                    <div className="media">
+                        <a className="thumbnail pull-left" href="#"> <img className="media-object" src={cartItem.imageUrl} style={{ width: "72px", height: "72px" }} /> </a>
+                        <div className="media-body">
+                            <h4 className="media-heading">{cartItem.name}</h4>
 
                         </div>
                     </div></td>
 
 
-                <td class="col-sm-1 col-md-1 text-center"><strong>${cartItem.price}</strong></td>
-                <td class="col-sm-1 col-md-1">
-                    <button onClick={() => removeFromCart(cartItem.id)} type="button" class="btn btn-danger">
-                        <span class="glyphicon glyphicon-remove"></span> Remove
+                <td className="col-sm-1 col-md-1 text-center"><strong>${cartItem.price}</strong></td>
+                <td className="col-sm-1 col-md-1">
+                    <button onClick={() => removeFromCart(cartItem.id)} type="button" className="btn btn-danger">
+                        <span className="glyphicon glyphicon-remove"></span> Remove
   </button></td>
             </tr>
 
@@ -44,33 +43,33 @@ function Cart() {
 
     return (
         <div>
-            <div class="container">
-                <div class="row">
-                    <div class="col-sm-12 col-md-10 col-md-offset-1">
-                        <table class="table table-hover">
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-12 col-md-10 col-md-offset-1">
+                        <table className="table table-hover">
                             <thead>
                                 <tr>
                                     <th>Product</th>
 
-                                    <th class="text-center">Price</th>
+                                    <th className="text-center">Price</th>
 
-                                    <th> </th>
+                                    <th> </th>
                                 </tr>
                             </thead>
                             <tbody>
                                 {renderCartItems()}
 
                                 <tr>
-                                    <td>   </td>
-                                    <td>   </td>
-                                    <td>   </td>
+                                    <td>   </td>
+                                    <td>   </td>
+                                    <td>   </td>
                                     <td>
-                                        <button type="button" class="btn btn-default">
-                                            <span class="glyphicon glyphicon-shopping-cart"></span> Continue Shopping
+                                        <button type="button" className="btn btn-default">
+                                            <span className="glyphicon glyphicon-shopping-cart"></span> Continue Shopping
                         </button></td>
                                     <td>
-                                        <button type="button" class="btn btn-success">
-                                            Checkout <span class="glyphicon glyphicon-play"></span>
+                                        <button type="button" className="btn btn-success">
+                                            Checkout <span className="glyphicon glyphicon-play"></span>
                                         </button></td>
                                 </tr>
                             </tbody>
